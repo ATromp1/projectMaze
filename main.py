2fe6f8939d5211dc6582735087fa879f6764e4b3
Flint101 <32782666+Flint101@users.noreply.github.com>
2022-07-15 15:37:47 +0200

    working base first walk

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ class Tile:
 
 
 class Grid:
-    def __init__(self, width, height, start, value=''):
+    def __init__(self, width, height, value=''):
         self.width = width
         self.height = height
+        self.direction = None
         self.data = np.ndarray(shape=(width, height), dtype=Tile)
         for col in range(height):
             for row in range(width):
                 self.data[row][col] = Tile(col, row, value)
 
-        self.current_tile = self.data[start[0]][start[1]]
-        self.history = []
+        self.history = [np.random.choice(self.data.ravel())]
+
+        #set starting point value
+        self.history[0].value = 'p'
+        # second starting point
+        np.random.choice(self.data.ravel()).value = 'x'
+
+
+
 
     def set(self, col, row, value):
         self.data[col][row].value = value
@@ -33,63 +41,87 @@ class Grid:
     def printGrid(self):
         print(self.data)
 
-    def move(self, tile):
-        choices = ['up', 'right', 'down', 'left']
-        ran = random.choice(choices)
+    def get_next_tile(self):
+        tile = self.history[-1]
+        direction = self.pick_direction(tile)
         row = tile.row
         col = tile.col
-        match ran:
+        print(direction)
+        match direction:
             case 'up':
-                print("up")
-                if row == 0:
-                    return
-
-                self.current_tile = self.data[row - 1][col]
-                print("current tile move - col", self.current_tile.col, "row", self.current_tile.row)
-                self.current_tile.value = 'p'
+                self.direction = direction
+                return self.data[row - 1][col]
 
             case 'right':
-                print("right")
-                if col == self.width - 1:
-                    return
-
-                self.current_tile = self.data[row][col + 1]
-                print("current tile move - col", self.current_tile.col, "row", self.current_tile.row)
-                self.current_tile.value = 'p'
+                self.direction = direction
+                return self.data[row][col + 1]
 
             case 'down':
-                print("down")
-                if row == self.height - 1:
-                    return
-
-                self.current_tile = self.data[row+1][col]
-                print("current tile move - col", self.current_tile.col, "row", self.current_tile.row)
-                self.current_tile.value = 'p'
+                self.direction = direction
+                return self.data[row + 1][col]
 
             case 'left':
-                print("left")
-                if col == 0:
-                    return
+                self.direction = direction
+                return self.data[row][col - 1]
 
-                self.current_tile = self.data[row][col-1]
-                print("current tile move - col", self.current_tile.col, "row", self.current_tile.row)
-                self.current_tile.value = 'p'
+    def move(self, next_tile):
+        self.check_loop(next_tile)
+        self.history.append(next_tile)
+        self.history[-1].value = 'p'
+
+    def pick_direction(self, tile):
+        direction = ['up', 'down', 'left', 'right']
+        if tile.row == 0:
+            direction = ['right', 'down', 'left']
+            if tile.col == self.width - 1:
+                direction = ['down', 'left']
+            if tile.col == 0:
+                direction = ['down', 'right']
+        elif tile.row == self.height - 1:
+            direction = ['up', 'right', 'left']
+            if tile.col == self.width - 1:
+                direction = ['up', 'left']
+            if tile.col == 0:
+                direction = ['up', 'right']
+        elif tile.col == self.width - 1:
+            direction = ['up', 'down', 'left']
+        elif tile.col == 0:
+            direction = ['up', 'right', 'down']
+
+        match self.direction:
+            case 'up':
+                direction.remove('down')
+            case 'down':
+                direction.remove('up')
+            case 'left':
+                direction.remove('right')
+            case 'right':
+                direction.remove('left')
+        return random.choice(direction)
+
+    def check_loop(self, next_tile):
+        if next_tile.value == 'p':
+            # print(self.history.index(next_tile))
+            x = np.where(self.data == next_tile)
+            # print("Grid index next tile", x)
+            # print("Which is:", self.data[x].tolist()[0].col, self.data[x].tolist()[0].row, self.data[x].tolist()[0].value)
+            history_index = self.history.index(self.data[x])
+            for element in reversed(self.history[history_index + 1:]):
+                self.data[element.row][element.col].value = 'w'
+            self.history = self.history[:history_index + 1]
+
+    def walk_path(self):
+        while True:
+            next_tile = self.get_next_tile()
+            if next_tile.value == 'x':
+                self.move(next_tile)
+                self.printGrid()
+                break
+            self.move(next_tile)
+            self.printGrid()
 
 
-gridSize = 4
-row = random.randint(0, gridSize - 1)
-col = random.randint(0, gridSize - 1)
-grid = Grid(gridSize, gridSize, (col, row), 'w')
-
-# pick a random starting point for the walk, I can make this a fixed starting point later if I want
-
-print("starting point is col", grid.current_tile.col, "row", grid.current_tile.row)
-
-if grid.get(col, row).value == 'w':
-    grid.set(col, row, 'p')
-
+gridSize = 6
+grid = Grid(gridSize, gridSize, 'w')
 grid.printGrid()
-
-for i in range(10):
-    grid.move(grid.current_tile)
-    grid.printGrid()
+grid.walk_path()
